20b78e8852abdc6bc9b901cd5e1565b582d56348
Jose Karol Tumulak <tumulakjkm@gmail.com>
2018-01-21 21:58:56 +0800

    added (empty) classes to Core.Generic in FuzzyLogicSystems

---
+++ FuzzyEvolution/FuzzyLogicSystem/Core/Generic/RuleBase/Simple3DRuleBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyLogicSystem.Core.Generic.RuleBase
+{
+    class Simple3DRuleBase : IFuzzyRuleBase
+    {
+    }
+}
