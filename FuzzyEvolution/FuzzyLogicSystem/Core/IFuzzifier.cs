4db715e3f9a4ae2cf8c7cdad38cc458e9f3e4826
Jose Karol Tumulak <tumulakjkm@gmail.com>
2018-01-21 21:00:12 +0800

    added (empty) Core interface of FuzzyLogicSystem

--- FuzzyEvolution/FuzzyLogicSystem/Core/IFuzzifier.cs
+++ FuzzyEvolution/FuzzyLogicSystem/Core/IFuzzifier.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FuzzyLogicSystem
+namespace FuzzyLogicSystem.Core
 {
-    public class Class1
+    interface IFuzzifier
     {
     }
 }
