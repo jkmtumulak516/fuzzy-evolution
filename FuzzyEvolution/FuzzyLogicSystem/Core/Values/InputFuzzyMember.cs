a77fe34908014754950ea7db0638350b8a9032e6
Jose Karol Tumulak <tumulakjkm@gmail.com>
2018-01-21 21:22:59 +0800

    made InputFuzzyMember and ResultFuzzyMember extend FuzzyMember in FuzzyLogicSystems

--- FuzzyEvolution/FuzzyLogicSystem/Core/Values/InputFuzzyMember.cs
+++ FuzzyEvolution/FuzzyLogicSystem/Core/Values/InputFuzzyMember.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FuzzyLogicSystem.Core.Values
 {
-    abstract class InputFuzzyMember
+    abstract class InputFuzzyMember : FuzzyMember
     {
     }
 }
--- FuzzyEvolution/FuzzyLogicSystem/Core/Values/ResultFuzzyMember.cs
+++ FuzzyEvolution/FuzzyLogicSystem/Core/Values/ResultFuzzyMember.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FuzzyLogicSystem.Core.Values
 {
-    abstract class ResultFuzzyMember
+    abstract class ResultFuzzyMember : FuzzyMember
     {
     }
 }
