04502da2cacf194a0887236a9939343f6a9b843f
Jose Karol Tumulak <tumulakjkm@gmail.com>
2018-01-21 23:31:24 +0800

    changed IFuzzySet to an abstract class from an interface
    renamed IFuzzySet to FuzzySet
    
    implemented FuzzySet

---
+++ FuzzyEvolution/FuzzyLogicSystem/Core/Values/FuzzySet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyLogicSystem.Core.Values
+{
+    public abstract class FuzzySet
+    {
+        private readonly int _category;
+        private readonly HashSet<IFuzzyMember> _members;
+
+        public FuzzySet(int category)
+        {
+            _category = category;
+            _members = new HashSet<IFuzzyMember>(InitializeMembers());
+        }
+
+        public int Category { get => _category; }
+        public HashSet<IFuzzyMember> Members { get => _members; }
+
+        protected abstract ISet<IFuzzyMember> InitializeMembers();
+    }
+}
