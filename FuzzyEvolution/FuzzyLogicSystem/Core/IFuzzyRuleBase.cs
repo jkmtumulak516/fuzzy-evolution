51140b9647d515c14b8435fb4469c49a181b8254
Jose Karol Tumulak <tumulakjkm@gmail.com>
2018-01-30 19:51:42 +0800

    implemented IFuzzyRuleBase

--- FuzzyEvolution/FuzzyLogicSystem/Core/IFuzzyRuleBase.cs
+++ FuzzyEvolution/FuzzyLogicSystem/Core/IFuzzyRuleBase.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using FuzzyLogicSystems.Core.Values;
 
 namespace FuzzyLogicSystems.Core
 {
     interface IFuzzyRuleBase
     {
+        List<FuzzyValue<ResultFuzzyMember>> Evaluate(IDictionary<int, IList<FuzzyValue<InputFuzzyMember>>> fuzzyValues);
     }
 }
