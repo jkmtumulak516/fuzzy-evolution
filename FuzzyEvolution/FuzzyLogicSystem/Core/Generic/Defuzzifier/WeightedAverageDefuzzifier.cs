da51f2ea1f9ead49734b145a8ea44e76550ce41b
Jose Karol Tumulak <tumulakjkm@gmail.com>
2018-01-30 22:33:24 +0800

    changed CenterOfGravityDefuzzifier to WeightedAverageDeffuzifier
    implemented CenterOfSumsDefuzzifier
    implemented WeightedAverageDefuzzifier

---
+++ FuzzyEvolution/FuzzyLogicSystem/Core/Generic/Defuzzifier/WeightedAverageDefuzzifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FuzzyLogicSystems.Core.Values;
+
+namespace FuzzyLogicSystems.Core.Generic.Defuzzifier
+{
+    class WeightedAverageDefuzzifier : IDefuzzifier
+    {
+        public float Defuzzify(IList<FuzzyValue<ResultFuzzyMember>> results)
+        {
+            float sum = 0.0f;
+
+            foreach (var fuzzyValue in results)
+                sum += fuzzyValue.Degree * fuzzyValue.FuzzyMember.Peak;
+
+            return sum / results.Count;
+        }
+    }
+}
