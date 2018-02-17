c346c611f668a27843d8196aec664c7501215537
Jose Karol Tumulak <tumulakjkm@gmail.com>
2018-02-18 00:51:38 +0800

    added (empty) interface to Genetic in EvolutionaryAlgorithms

--- FuzzyEvolution/EvolutionaryAlgorithms/Genetic/IMutatable.cs
+++ FuzzyEvolution/EvolutionaryAlgorithms/Genetic/IMutatable.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EvolutionaryAlgorithms
+namespace EvolutionaryAlgorithms.Genetic
 {
-    public class Class1
+    interface IMutatable
     {
     }
 }
