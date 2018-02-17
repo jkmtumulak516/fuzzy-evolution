30ded462ee93c8226f9a8b89bf65e628beeed1da
Jose Karol Tumulak <tumulakjkm@gmail.com>
2018-02-18 01:18:52 +0800

    cleaned using in Genetic of EvolutionaryAlgorithms

--- FuzzyEvolution/EvolutionaryAlgorithms/Genetic/ISelectionMethod.cs
+++ FuzzyEvolution/EvolutionaryAlgorithms/Genetic/ISelectionMethod.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EvolutionaryAlgorithms.Genetic
 {
     public interface ISelectionMethod
     {
-        IList<Tuple<IOrganism, IOrganism>>  SelectPairings(IList<IOrganism> population);
+        IList<Tuple<IOrganism, IOrganism>> SelectPairings(IList<IOrganism> population);
     }
 }
