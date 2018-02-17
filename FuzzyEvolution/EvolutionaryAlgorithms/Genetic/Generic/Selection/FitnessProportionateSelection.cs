2b749ddf4597dc375b3bea7a4ea9e3737c93abb7
Jose Karol Tumulak <tumulakjkm@gmail.com>
2018-02-18 01:49:42 +0800

    corrected spelling error in Generic class FitnessProportionateSelection

--- FuzzyEvolution/EvolutionaryAlgorithms/Genetic/Generic/Selection/FitnessProportionateSelection.cs
+++ FuzzyEvolution/EvolutionaryAlgorithms/Genetic/Generic/Selection/FitnessProportionateSelection.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EvolutionaryAlgorithms.Genetic.Generic.Selection
 {
-    class FitenessProportionateSelection
+    class FitnessProportionateSelection
     {
     }
 }
